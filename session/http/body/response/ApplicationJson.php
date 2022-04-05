e63fe96b25d1af2e2d2894e421c9f74244a8564a
DmitryP <kernins@gmail.com>
2022-04-05 23:34:06 +0300

    minor fix/improvement

--- session/http/body/response/ApplicationJson.php
+++ session/http/body/response/ApplicationJson.php
@@ -1,11 +1,18 @@
 <?php
 namespace lib\dp\Curl\session\http\body\response;
+use lib\dp\Curl\session\http\body\Response as ResponseBase;
 
 
-class ApplicationJson extends Text
+/**
+ * PHP's json_decode() only works with UTF-8
+ */
+class ApplicationJson extends ResponseBase
    {
-      public function getData()
+      public function getData(): ?array
          {
-            return json_decode(parent::getData(), true, 512, JSON_THROW_ON_ERROR);
+            return json_decode(
+               parent::getData(),
+               flags: JSON_THROW_ON_ERROR | JSON_OBJECT_AS_ARRAY
+            );
          }
    }
